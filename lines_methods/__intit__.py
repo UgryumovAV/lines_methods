8c848f636ec26ed074e373d69ab168df23fd5248
Andrey Laptop <ugryumov2102@yandex.ru>
2024-12-16 23:34:35 +0300

    test fix

--- lines_methods/__intit__.py
+++ lines_methods/__intit__.py
@@ -1,9 +1,9 @@
-from lines_methods import (
-    get_intersection,
-    get_length,
-    get_angle_between_lines,
-    extend_line,
-    get_medium_point_from_line,
-    angle_between_vector_and_horizontal,
-    create_vector_from_point_with_angle_to_vector
-)
+# from lines_methods import (
+#     get_intersection,
+#     get_length,
+#     get_angle_between_lines,
+#     extend_line,
+#     get_medium_point_from_line,
+#     angle_between_vector_and_horizontal,
+#     create_vector_from_point_with_angle_to_vector
+# )
